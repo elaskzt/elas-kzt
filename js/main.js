4423bed26bd5144df7d93ceef2af76de6cb8196c
AlbertRAR <behappyhappybee@gmail.com>
2015-01-15 09:56:11 +0300

    portfolio page

--- js/main.js
+++ js/main.js
@@ -1,6 +1,21 @@
 $(document).ready(function() {
 
-	
+	$(function(){
+
+		$('#Container').mixItUp();
+
+	});
+
+	$(function() {
+		$(".tags-block button").click(function() {
+		  // remove classes from all
+		  $(".tags-block button").removeClass("active");
+		  // add class to the one we clicked
+		  $(this).addClass("active");
+		  // stop the page from jumping to the top
+		  return false;
+		});
+	});
 
 });
 
